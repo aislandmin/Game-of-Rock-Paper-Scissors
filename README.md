f4cf3331770c428a31e3ec1da8d53fc2a9d5a0ad
Xiaomin Guo <aislandmin@hotmail.com>
2022-08-05 13:39:47 -0400

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+# Game of Rock, Paper, Scissors
+
+This is a solution to Game of Rock, Paper, Scissors.
+
+The Users should be able to:
+
+- View the optimal layout for the game depending on their device's screen size
+- Play Rock, Paper, Scissors against the computer
+
+## Rules
+
+If the player wins, the player gains 1 point. If the computer wins, the player loses one point.
+
+- Paper beats Rock
+- Rock beats Scissors
+- Scissors beats Paper
+
+
+## Table of contents
+
+- [Overview](#overview)
+  - [Link](#link)
+  - [Screenshots](#screenshots)
+- [My process](#my-process)
+  - [Built with](#built-with)
+- [Author](#author)
+
+## Overview
+
+### Link
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+
+### Screenshots
+
+![](./screenshots/screenshot1.png)
+
+![](./screenshots/screenshot2.png)
+
+![](./screenshots/screenshot3.png)
+
+![](./screenshots/screenshot4.png)
+
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- jQuery
+
+### Useful resources
+
+- [box-shadow](https://css-tricks.com/almanac/properties/b/box-shadow/) 
+
+## Author
+
+- Website - [Xiaomin Guo](https://min-website-aislandmin.vercel.app/)
+
---
+++ src/index.js
@@ -0,0 +1,64 @@
+const choiceList = ["rock", "paper", "scissors"];
+let gameScore = 0;
+
+$("#btn-game-ruler").click(function () {
+  $(".game-rules").addClass("help");
+});
+
+$("#gamebtn-close").click(function () {
+  $(".game-rules").removeClass("help");
+});
+
+const computerSelection = function () {
+  let randomIndex = Math.floor(Math.random() * choiceList.length);
+  let pickedChoice = choiceList[randomIndex];
+  $(`#${pickedChoice}`).clone().appendTo("#computer-side");
+  return randomIndex;
+};
+
+const playPressedSound = function () {
+  let audio = new Audio("../sound/pressed.wav");
+  audio.play();
+};
+
+const playWinSound = function () {
+  let audio = new Audio("../sound/win.wav");
+  audio.play();
+};
+
+const playLoseSound = function () {
+  let audio = new Audio("../sound/lose.wav");
+  audio.play();
+};
+
+const playerSelection = function (key) {
+  key.clone().appendTo("#player-side");
+  let playerChoice = choiceList.indexOf(key.attr("id"));
+  let computerChoice = computerSelection();
+  if (playerChoice === computerChoice) {
+    $("#game-status").text("DRAW");
+  } else {
+    if (computerChoice === (playerChoice + 1) % 3) {
+      $("#game-status").text("YOU LOSE");
+      playLoseSound();
+      $("#computer-side div").addClass("winner-effect");
+    } else {
+      $("#game-status").text("YOU WIN");
+      playWinSound();
+      $("#player-side div").addClass("winner-effect");
+      $(".score-number").text(`${++gameScore}`);
+    }
+  }
+};
+
+$(".choice-wrapper").click(function (event) {
+  playPressedSound();
+  $(".game-container").addClass("toggle");
+  let choice = $(event.currentTarget);
+  playerSelection(choice);
+  $("#game-startover").click(function () {
+    $("#player-side div").remove();
+    $("#computer-side div").remove();
+    $(".game-container").removeClass("toggle");
+  });
+});
